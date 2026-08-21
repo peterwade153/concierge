f3cdcc212595ed8d59a97d0ff88b2f8fed98611c
peterwade153 <peterwade153@gmail.com>
2026-08-21 10:10:43 +0300

    update raised error message for missing key

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from agent.openrouter_agent import RestaurantAgent as RA
 
 
 if __name__ == "__main__":
-    # Ensure GEMINI_API_KEY environment variable is set before running
-    if not os.environ.get("GEMINI_API_KEY"):
-        raise ValueError("Please set your GEMINI_API_KEY environment variable.")
+    if not os.environ.get("OPENROUTER_API_KEY"):
+        raise ValueError("Please set your OPENROUTER_API_KEY environment variable.")
 
     concierge = RA()
     
-    # Execution Test Case: Multi-turn filtering logic
     concierge.ask("Find me a good spot for Asian food around the Ntinda or Nakawa neighborhood.")
